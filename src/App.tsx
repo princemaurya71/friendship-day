c65074c0e638249675a11f9e78f8712f628a410b
princemaurya71 <mr.prince3650@gmail.com>
2026-08-02 16:12:48 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import FriendshipCard from './components/FriendshipCard';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return <>
